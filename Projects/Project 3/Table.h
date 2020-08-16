0a84182c4f9bc01446684a82f1476baadf1f588f
Valentinno Cruz <valentinnocruz@gmail.com>
2020-08-16 09:11:05 -0800

    push Project 3

---
+++ Projects/Project 3/Table.h
@@ -0,0 +1,34 @@
+
+
+#ifndef TABLE_H
+#define	TABLE_H
+#include <iostream>
+using namespace std;
+struct table{
+  char **fake;
+  char **real;
+};
+class Table {
+public:
+  //Constructor
+  Table(int);
+  int getNum() const{
+    return num;
+  }
+  //return one element of table
+  char getTable(int x, int y){
+    return t->real[x][y];
+  }
+  void setTable(int x, int y, char i){
+    t->fake[x][y]=i;
+    t->real[x][y]=i;
+  }
+  //Destructor
+  virtual ~Table();
+protected:
+  int num;
+  table *t;
+};
+#endif	/* TABLE_H */
+
+
